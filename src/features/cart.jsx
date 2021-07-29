d40b7d167f19f9c99bb24d2fedb75aecbcfc3cda
Vidhi Pandey <vidhi.pandey@khatabook.com>
2021-07-29 18:45:14 +0530

    initial commit and mui added

---
+++ src/features/cart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CartPage = () => {
+  return (
+    <div style={{ padding: "5%" }}>
+      <h1>Order Summary</h1>
+    </div>
+  );
+};
+
+export default CartPage;
